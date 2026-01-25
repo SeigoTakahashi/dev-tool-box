28c41aeda056cfd6ce3a46c051d36969ef2f5144
Seigo Takahashi(髙橋清吾) <112990442+SeigoTakahashi@users.noreply.github.com>
2026-01-25 11:46:35 +0900

    ランダム生成機能実装 (#28)

---
+++ src/features/utility/components/RandomGenerator.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Box, Button } from "@mui/material";
+import CopyButton from "../../../common/components/CopyButton";
+import {
+  generateUUID,
+  generateSecurePassword,
+} from "../utils/random-generator";
+
+// ランダム生成コンポーネント
+const RandomGenerator = () => {
+  const [uuid, setUuid] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+        alignItems: "center",
+      }}
+    >
+      <Button
+        variant="contained"
+        sx={{ m: 2, width: 200 }}
+        onClick={() => setUuid(generateUUID())}
+      >
+        UUID生成
+      </Button>
+      {uuid && (
+        <>
+          <Box sx={{ wordBreak: "break-all" }}>{uuid}</Box>
+          <CopyButton text={uuid} />
+        </>
+      )}
+      <Button
+        variant="contained"
+        sx={{ m: 2, width: 200 }}
+        onClick={() => setPassword(generateSecurePassword())}
+      >
+        パスワード生成
+      </Button>
+      {password && (
+        <>
+          <Box sx={{ wordBreak: "break-all" }}>{password}</Box>
+          <CopyButton text={password} />
+        </>
+      )}
+    </Box>
+  );
+};
+
+export default RandomGenerator;
